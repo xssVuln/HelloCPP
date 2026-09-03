19a2a59686674d1824b6d5b5e08fd7e072be0316
xssVuln <shunyang.1882674@nv.ccsd.net>
2026-09-03 21:12:43 +0000

    Add introductory C++ examples and VS Code configuration

---
+++ HelloInput/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int num = 100;
+    cout << "Enter a number: ";
+    cin >> num;
+    cout << "You entered: " << num << endl;
+    return 0;
+}
